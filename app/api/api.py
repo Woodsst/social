4f9a578f1e749cad1e476dae58512f7c23ab07c4
wood <amper.sil.tok@gmail.com>
2023-02-15 06:49:46 +0300

    Fixed dependencies and imports

--- app/api/api.py
+++ app/api/api.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter
 
 from api.endpoints.posts.view import posts
 from api.endpoints.reaction.view import user_reaction
-from app.api.endpoints.login.view import login_router
-from app.api.endpoints.registration.view import registration_router
-from app.api.endpoints.user_page.view import user_router
+from api.endpoints.login.view import login_router
+from api.endpoints.registration.view import registration_router
+from api.endpoints.user_page.view import user_router
 
 api_router = APIRouter()
 
