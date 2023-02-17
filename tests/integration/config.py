e4bba34a392cfeae73c85b52297824778df217cd
wood <amper.sil.tok@gmail.com>
2023-02-17 14:16:59 +0300

    Added dockerfile for test

---
+++ tests/integration/config.py
@@ -0,0 +1,20 @@
+from functools import lru_cache
+
+from pydantic import BaseSettings, Field
+
+
+class Settings(BaseSettings):
+    """Settings."""
+
+    postgres_dsn: str = Field("postgresql+asyncpg://app:123@localhost/social")
+    jwt_algorithm: str = "HS256"
+    jwt_access_secret: str = Field("super secret string for access token")
+    jwt_refresh_secret: str = Field("super secret string for refresh token")
+    jwt_access_lifetime: int = Field(1)
+    jwt_refresh_lifetime: int = Field(14)
+
+
+@lru_cache()
+def get_settings():
+    """Settings factory."""
+    return Settings()
